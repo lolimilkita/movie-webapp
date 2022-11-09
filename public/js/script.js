f2d884d7702471fe091f6632d1805c632c0c60ad
lolimilkita <caturcno.cr@gmail.com>
2022-11-09 23:42:42 +0700

    Add modal for see detail movie, & search on enter

--- public/js/script.js
+++ public/js/script.js
@@ -1,50 +1,127 @@
-$("#button-search").on("click", function () {
+function searchMovie() {
+    $("#movie-list").html("");
+
     $.ajax({
-        url: "http://www.omdbapi.com/",
+        url: "http://omdbapi.com",
         type: "get",
         dataType: "json",
         data: {
             apikey: "e31057e7",
-            s: $("#input-search").val(),
+            s: $("#search-input").val(),
         },
         success: function (result) {
             // console.log(result);
             if (result.Response == "True") {
                 let movies = result.Search;
                 // console.log(movies);
+
                 $.each(movies, function (i, data) {
                     $("#movie-list").append(
                         `
                         <div class="col-md-4">
-                            <div class="card mb-3" >
+                            <div class="card mb-3">
                                 <img src="` +
                             data.Poster +
                             `" class="card-img-top" alt="...">
                                 <div class="card-body">
-                                <h5 class="card-title">` +
+                                    <h5 class="card-title">` +
                             data.Title +
                             `</h5>
-                                <h6 class="card-subtitle mb-2 text-muted">` +
+                                    <h6 class="card-subtitle mb-2 text-muted">` +
                             data.Year +
                             `</h6>
-                                <a href="#" class="card-link">Detail</a>
+                                    <a href="#" class="card-link see-detail" data-bs-toggle="modal" data-bs-target="#exampleModal" data-id="` +
+                            data.imdbID +
+                            `">See detail</a>
                                 </div>
                             </div>
                         </div>
                     `
                     );
                 });
+                $("#search-input").val("");
             } else {
                 $("#movie-list").html(
                     `
-                    <h1 class="text-center">` +
+                    <h3 class="text-center">` +
                         result.Error +
-                        `</h1>
+                        `</h3>
+                `
+                );
+            }
+        },
+    });
+}
+
+$("#search-btn").on("click", function () {
+    searchMovie();
+});
+
+$("#search-input").on("keyup", function (e) {
+    // jika yg ditekan enter
+    if (e.keyCode === 13) {
+        searchMovie();
+    }
+});
+
+$("#movie-list").on("click", ".see-detail", function () {
+    // console.log($(this).data('id'));
+
+    $.ajax({
+        url: "http://omdbapi.com",
+        type: "get",
+        dataType: "json",
+        data: {
+            apikey: "e31057e7",
+            i: $(this).data("id"),
+        },
+        success: function (movie) {
+            // console.log(result);
+            if (movie.Response == "True") {
+                $(".modal-body").html(
+                    `
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <img src="` +
+                        movie.Poster +
+                        `" class="img-fluid">
+                            </div>
+                            <div class="col-md-8">
+                                <ul class="list-group">
+                                    <li class="list-group-item"><h3>` +
+                        movie.Title +
+                        `</h3></li>
+                                    <li class="list-group-item">Rating : ` +
+                        movie.imdbRating +
+                        `</li>
+                                    <li class="list-group-item">Released : ` +
+                        movie.Released +
+                        `</li>
+                                    <li class="list-group-item">Genre : ` +
+                        movie.Genre +
+                        `</li>
+                                    <li class="list-group-item">Director : ` +
+                        movie.Director +
+                        `</li>
+                                    <li class="list-group-item">Actors : ` +
+                        movie.Actors +
+                        `</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                `
+                );
+            } else {
+                $("#movie-list").html(
+                    `
+                    <h3 class="text-center">` +
+                        result.Error +
+                        `</h3>
                 `
                 );
             }
         },
     });
 });
-
-// $("#button-search").on("click", console.log("haloo ges"));
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,8 +9,8 @@
             <h4>Search Movie</h4>
         
             <div class="input-group mb-3">
-                <input type="text" class="form-control" placeholder="Movie title...." id="input-search">
-                <button class="btn btn-outline-dark" type="button" id="button-search">Button</button>
+                <input type="text" class="form-control" placeholder="Movie title.." id="search-input">
+                <button class="btn btn-dark" type="button" id="search-btn">Search</button>
             </div>
         </div>
     </div>
@@ -19,6 +19,24 @@
 
     <div class="row" id="movie-list">
 
+    </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+            ...
+            </div>
+            <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            </div>
+        </div>
+        </div>
     </div>
 
 @endsection
